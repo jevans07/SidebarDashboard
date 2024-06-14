bdea1eaf6f865c20af5b2764a61168e60de54107
jevans07 <jevans26@villanova.edu>
2024-06-14 09:26:55 -0400

    Add files via upload

---
+++ pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <header>
+        <h1>Antimicrobial Resistance Homepage</h1>
+      </header>
+      <main>
+        <p>This is where we will be able to give a brief overview of what the website is about and can direct our users 
+            to other links where they will be able to gain the information they are searching for.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default Home;

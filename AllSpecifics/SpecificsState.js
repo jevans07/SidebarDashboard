9e684fc84cf93ac04fd1ff6ffc6196e00847c0c0
jevans07 <jevans26@villanova.edu>
2024-06-14 09:09:02 -0400

    Add files via upload

---
+++ AllSpecifics/SpecificsState.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpecificsState = () => {
+  return (
+    <div className='specifics'>
+      <h1> Specifics: States </h1>
+    </div>
+  )
+}
+
+export default SpecificsState
